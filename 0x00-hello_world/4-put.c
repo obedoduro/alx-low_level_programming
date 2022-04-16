63bb51904558c85343928f79b73269b3bb77e3b3
obedoduro <oduroadjeiobed16@gmail.com>
2022-04-16 10:31:51 -0700

    commit

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: prints c programming is like building a multilingual puzzle
+*
+* Return: 0
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
