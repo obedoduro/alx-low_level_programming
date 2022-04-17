258844a43620d415e053909cc14ac4be0e051196
obedoduro <oduroadjeiobed16@gmail.com>
2022-04-17 14:46:50 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,28 +1,23 @@
-#include "main.h"
-
-/**
-* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-* followed by a new line
-*/
-void print_alphabet_x10(void)
-{
-char ch;
-int i;
-i = 0;
-while (i < 10)
-{
-ch = 'a';
-while (ch <= 'z')
-{
-_putchar(ch);
-ch++;
-}
-_putchar('\n');
-i++;
-}
-}
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
+#include "main.h"                                                                                      
+                                                                                                       
+/**                                                                                                    
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,                                     
+* followed by a new line                                                                               
+*/                                                                                                     
+void print_alphabet_x10(void)                                                                          
+{                                                                                                      
+char ch;                                                                                               
+int i;                                                                                                 
+i = 0;                                                                                                 
+while (i < 10)                                                                                         
+{                                                                                                      
+ch = 'a';                                                                                              
+while (ch <= 'z')                                                                                      
+{                                                                                                      
+_putchar(ch);                                                                                          
+ch++;                                                                                                  
+}                                                                                                      
+_putchar('\n');                                                                                        
+i++;                                                                                                   
+}                                                                                                      
+}          
