5135417747b69860c50f10a2f5fe35153ca5c1b7
obedoduro <oduroadjeiobed16@gmail.com>
2022-04-23 04:14:53 -0700

    pointerandarrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strcat - fills memory with a constant byte
+* @d: memory area to be filled
+* @c: character to copy
+* @rc: number of times to copy c
+* Return: pointer to the memory of d
+*/
+char *_strcat(char *d, char c, unsigned int rc)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+d[i] = c;
+}
+return (d);
+}
