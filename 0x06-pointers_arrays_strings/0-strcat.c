2f391b1629e9c010903d45cc4d22f64c36cc12d6
oduro adjei obed <oduroadjeiobed16@gmail.com>
2022-04-23 11:24:07 +0000

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,22 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
-* _strcat - fills memory with a constant byte
-* @d: memory area to be filled
-* @c: character to copy
-* @rc: number of times to copy c
-* Return: pointer to the memory of d
-*/
-char *_memset(char *d, char c, unsigned int rc)
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-d[i] = c;
-}
-return (d);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
