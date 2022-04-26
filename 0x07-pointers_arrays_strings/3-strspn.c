596e4ca00325997fd71ecc04ea8ac6b9ebd2b0e8
oduro adjei obed <oduroadjeiobed16@gmail.com>
2022-04-26 23:19:45 +0000

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,22 +8,22 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-int i, j, f, flag;
-f = 0;
+int i, j, t, p;
+t = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
-flag = 0;
+p = 0;
 for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
 {
-f++;
-flag = 1;
+t++;
+p = 1;
 }
 }
-if (flag == 0)
+if (p == 0)
 {
-return (f);
+return (t);
 }
 }
 return (0);
